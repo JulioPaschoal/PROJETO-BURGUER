0820cda4ffed91925e4447a49618348b04b72c63
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-04-02 20:29:07 -0300

    cadastrando usuario e validado os dados

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,46 @@
+import { v4 } from 'uuid';
+import User from '../models/User';
+import * as Yup from 'yup';
+
+class UserController {
+  // Store a new user \\
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .min(5, 'O nome deve conter no mínimo 5 dígitos!')
+        .required('Insira um nome completo!'),
+      email: Yup.string()
+        .email('Insira um e-mail valido!')
+        .required('O campo e-mail é obrigatório!'),
+      password_hash: Yup.string()
+        .min(6, 'A senha deve conter no mínimo 6 dígitos!')
+        .required('Ocampo senha é obrigatória!'),
+      admin: Yup.boolean(),
+    });
+    try {
+      schema.validateSync(req.body, { abortEarly: false });
+    } catch (error) {
+      return res.status(400).json({ error: error.errors });
+    }
+    const { name, email, password_hash, admin } = req.body;
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'O usuário já existe no sistema!' });
+    }
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
+    return res.status(201).json({
+      id: user.id,
+      name,
+      email,
+      admin,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,7 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
+import UserController from './app/controllers/UserController';
 const routes = new Router();
 
-import User from './app/models/User';
-
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Julio Paschoal',
-    email: 'juliocpaschoal.com',
-    password_hash: 'sd84d8946wd49',
-    admin: true,
-  });
-  return res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
