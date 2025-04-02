f6da3acca787a91a0d473ff76929ba1e7cb1c879
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-04-02 16:16:22 -0300

    creando model de User e gravando no banco

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Welcome to the API!' });
+import User from './app/models/User';
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Julio Paschoal',
+    email: 'juliocpaschoal.com',
+    password_hash: 'sd84d8946wd49',
+    admin: true,
+  });
+  return res.status(201).json(user);
 });
 
 export default routes;
