93c78235826e9aaf250c9fe69b1f266db8b2095a
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-04-03 17:22:36 -0300

    criando migration de products

---
+++ src/database/migrations/20250403201633-create-products-table.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  },
+};
