dd85f6e39fd3e88e795a7b6d6acd888cf1b02efa
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-04-05 11:17:18 -0300

    salvando path no banco de dados e recuperando imagem do banco

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import { v4 } from 'uuid';
+import Product from '../models/Product';
 
 class ProductController {
   async store(req, res) {
@@ -7,14 +8,27 @@ class ProductController {
       name: Yup.string().required('O nome é obrigatório!'),
       price: Yup.number().required('O preco é obrigatório!'),
       category: Yup.string().required('A categoria é obrigatória!'),
-      //path: Yup.string().required('O path é obrigatório!'),
     });
     try {
       schema.validateSync(req.body, { abortEarly: false });
     } catch (error) {
       return res.status(400).json({ error: error.errors });
     }
-    return res.status(201).json({ message: 'ok' });
+    const { filename: path } = req.file;
+    const { name, price, category } = req.body;
+    const product = await Product.create({
+      id: v4(),
+      name,
+      price,
+      category,
+      path,
+    });
+
+    return res.status(201).json(product);
+  }
+  async index(req, res) {
+    const products = await Product.findAll();
+    return res.json(products);
   }
 }
 
