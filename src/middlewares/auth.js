2ef03cd821edc961c460b6ff6d7bb2e958ab8f71
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2025-04-05 16:08:47 -0300

    criando middleware e usando token JWT

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+function authMiddleware(req, res, next) {
+  const authToken = req.headers.authorization;
+  if (!authToken) {
+    return res.status(401).json({ error: 'Você precisa estar logado!' });
+  }
+  const [_, token] = authToken.split(' ');
+  try {
+    jwt.verify(token, process.env.APP_SECRET, (err, decoded) => {
+      if (err) {
+        throw new Error();
+      }
+      req.userId = decoded.id;
+      return next();
+    });
+  } catch (err) {
+    return res.status(401).json({ error: 'Token inválido!' });
+  }
+}
+
+export default authMiddleware;
